adb7a9fdd0285da0e5d39e264ce794bcd26a8e41
Alexio <alexioamota@gmail.com>
2016-05-09 11:05:41 -0700

    Added quiz flow to quiz app

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -10,8 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let questions: [String] = ["From what is cognac made?",
+                               "What is 7+7?",
+                               "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes",
+                             "14",
+                             "Montpelier"]
+    var currentQuestionIndex: Int = 0
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        answerLabel.text = answers[currentQuestionIndex]
+    }
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        currentQuestionIndex+=1
+        if (currentQuestionIndex >= questions.count) {
+            currentQuestionIndex = 0
+        }
+        answerLabel.text = "???"
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +44,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
+
 
