02180e4795f5fd4b1dbc40d9605145a7c84a7084
Alexio <alexioamota@gmail.com>
2016-05-10 22:24:07 -0700

    Add item addition, deletion, and moving

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -12,6 +12,34 @@ class ItemsViewController: UITableViewController {
 
     var itemStore: ItemStore!
     
+    @IBAction func addNewItem(sender: AnyObject) {
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.indexOf(newItem) {
+            let indexPath = NSIndexPath(forRow: index, inSection: 0)
+            // Insert this new row into the table
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(sender: AnyObject) {
+        // If you are currently in editing mode...
+        if editing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", forState: .Normal)
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        }
+        else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", forState: .Normal)
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,8 +61,7 @@ class ItemsViewController: UITableViewController {
         return itemStore.allItems.count
     }
     
-    override func tableView(tableView: UITableView,
-                            cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         // Get a new or recycled cell
         let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         // Set the text on the cell with the description of the item
@@ -45,6 +72,39 @@ class ItemsViewController: UITableViewController {
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         return cell
     }
-
+    
+    override func tableView(tableView: UITableView,commitEditingStyle editingStyle: UITableViewCellEditingStyle,
+                            forRowAtIndexPath indexPath: NSIndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .Delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Remove \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            let ac = UIAlertController(title: title,
+                                       message: message,
+                                       preferredStyle: .ActionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            ac.addAction(cancelAction)
+            let deleteAction = UIAlertAction(title: "Delete", style: .Destructive,
+                                             handler: { (action) -> Void in
+                                                // Remove the item from the store 
+                                                self.itemStore.removeItem(item)
+                                                // Also remove that row from the table view with an animation
+                                                self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic) })
+            ac.addAction(deleteAction)
+            
+            // Present the alert controller
+            presentViewController(ac, animated: true, completion: nil)
+        }
+    }
+    
+    override func tableView(tableView: UITableView,
+                            moveRowAtIndexPath sourceIndexPath: NSIndexPath,
+                                               toIndexPath destinationIndexPath: NSIndexPath) {
+        // Update the model
+        itemStore.moveItemAtIndex(sourceIndexPath.row, toIndex: destinationIndexPath.row)
+    }
 }
 
