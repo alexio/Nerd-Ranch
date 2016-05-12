641eaf62ec50d0944f3c0d08b2d20a0924e04993
Alexio <alexioamota@gmail.com>
2016-05-12 15:55:22 -0700

    Add multi line drawing

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Alexio Mota on 5/12/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
